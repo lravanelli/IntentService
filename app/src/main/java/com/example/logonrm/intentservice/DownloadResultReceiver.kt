f6325cd204aae0e1de6cda54e3b0492f63dceaae
lravanelli <ravanelli.luis@gmail.com>
2018-02-19 15:33:41 -0300

    Correções

--- app/src/main/java/com/example/logonrm/intentservice/DownloadResultReceiver.kt
+++ app/src/main/java/com/example/logonrm/intentservice/DownloadResultReceiver.kt
@@ -6,17 +6,18 @@ import android.os.ResultReceiver
 
 class DownloadResultReceiver(handler: Handler) : ResultReceiver(handler) {
     private var mReceiver: Receiver? = null
+
     fun setReceiver(receiver: Receiver){
         mReceiver = receiver
     }
 
     interface Receiver {
-        fun onReceiveResult(resultCode: Int, resultData: Bundle)
+        fun onReceiverResult(resultCode: Int, resultData: Bundle)
     }
 
     override fun onReceiveResult(resultCode: Int, resultData: Bundle) {
         if(mReceiver != null){
-            mReceiver!!.onReceiveResult(resultCode, resultData)
+            mReceiver!!.onReceiverResult(resultCode, resultData)
         }
     }
 
